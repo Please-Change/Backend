edd2ee12ef40d9f0237f24d3f093de53d48c3c2d
Ruaidhri O'Shea <124783806+ros40@users.noreply.github.com>
2024-04-13 14:45:20 +0100

    Basic websocket setup

---
+++ pkg/server/game.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+func handleGame(w http.ResponseWriter, r *http.Request) {
+	var upgrade = websocket.Upgrader{
+		ReadBufferSize:  512,
+		WriteBufferSize: 512,
+		WriteBufferPool: &sync.Pool{},
+	}
+
+	conn, err := upgrade.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println("Couldn't upgrade connection: ", err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		mt, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read failed:", err)
+			break
+		}
+		input := string(message)
+		message = []byte(input)
+		err = conn.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write failed:", err)
+			break
+		}
+	}
+}
